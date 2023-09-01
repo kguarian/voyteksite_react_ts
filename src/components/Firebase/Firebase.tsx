8bab827dd391a550438769b58371e59829491305
Kenton Guarian <kguarian@ucsd.edu>
2023-09-01 14:00:36 -0700

    only 8 plots, select broken, no update, react

--- src/components/Firebase/Firebase.tsx
+++ src/components/Firebase/Firebase.tsx
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 import "firebase/analytics";
 import { DataSnapshot, get, getDatabase, ref } from "firebase/database";
 import Plot from "react-plotly.js";
-import Plotly from "plotly.js";
+import Plotly, { Layout } from "plotly.js";
 import { data } from "jquery";
 
 const initializeFirebase = () => {
@@ -23,20 +23,39 @@ const initializeFirebase = () => {
   // getAnalytics(appreturn);
 };
 
+interface myState {
+  plots: any[];
+}
+const layout_params: any={
+// Layout
+dragmode: 'select',
+selectdirection: "h",
+xaxis: { zeroline: false },
+showlegend: true,
+width: 1000,
+height: 150,
+margin: {
+    l: 25,
+    r: 25,
+    b: 25,
+    t: 25,
+    pad: 5
+  }
+}
+
 class FirebaseComponent extends React.Component {
   constructor(props: any) {
     super(props);
     this.state = {
-      plots: [],
+      plots: [] as any[],
     };
-    this.getSigCount();
-    console.log(this.state);
+    this.initializePage();
   }
 
   initializePage() {
     let sigs, users;
     let sig_refs: string[] = [];
-    let test_sigs: number = 2;
+    let test_sigs: number = 9 ;
     this.getSigCount().then((num_sigs) => {
       console.log(num_sigs);
       if (num_sigs == 0) {
@@ -46,7 +65,58 @@ class FirebaseComponent extends React.Component {
       const sig_idxs = this.pick_sigs(test_sigs, num_sigs);
       const sig_refs = this.refs_from_idxs(sig_idxs);
       console.log(sig_refs);
-      this.getSigs(sig_refs);
+      const dataPromise = this.getSigs(sig_refs);
+      dataPromise.then((data) => {
+        const x_list = [];
+        const y_list = [...data];
+
+        if (y_list.length != test_sigs) {
+          console.log("wrong number of sigs");
+          return;
+        }
+        for (let i = 0; i < y_list.length; i++) {
+          x_list.push(this.make_new_x_burst(0, y_list[i].length));
+        }
+        console.log(x_list);
+        console.log(y_list);
+        const plots: any[] = [];
+        const plotly_burst: any[] = [];
+        const plotly_before: any[] = [];
+        const plotly_after: any[] = [];
+        for (let i = 0; i < x_list.length; i++) {
+          const data_elements_before = {
+            x: [],
+            y: [],
+            type: "scattergl",
+            name: `sig_${i}`,
+            marker: { color: "black" },
+          };
+          const data_elements_burst = {
+            x: x_list[i],
+            y: y_list[i],
+            type: "scattergl",
+            name: `sig_${i}`,
+            marker: { color: "red" },
+          };
+          const data_elements_after = {
+            x: [],
+            y: [],
+            type: "scattergl",
+            name: `sig_${i}`,
+            marker: { color: "black" },
+          };
+
+          plotly_burst.push([
+            data_elements_before,
+            data_elements_burst,
+            data_elements_after,
+          ]);
+        }
+        console.log(plotly_burst.length);
+        this.setState({
+          plots: plotly_burst,
+        });
+      });
     });
   }
   async getSigCount(): Promise<number> {
@@ -58,7 +128,7 @@ class FirebaseComponent extends React.Component {
       const num_sigs = snapshot.val();
       return snapshot.val();
     } else {
-      console.log("No data available");
+      console.log("n_sig data unavailable from firebase");
       return 0;
     }
   }
@@ -105,32 +175,124 @@ class FirebaseComponent extends React.Component {
     return sig_refs;
   }
 
-  async getSigs(sig_refs: string[]) {
+  async getSigs(sig_refs: string[]): Promise<number[][]> {
     console.log("running callback");
-    const values: DataSnapshot[] = [];
+    const values: number[][] = [];
     // get all json data for each sig
     const promise_array = [];
     for (let i = 0; i < sig_refs.length; i++) {
-      const dbref = ref(getDatabase(), sig_refs[i]);
+      const dbref = ref(getDatabase(), `sigs/${sig_refs[i]}`);
       promise_array.push(get(dbref));
     }
-    Promise.all(promise_array).then((snapshots) => {
-      console.log(snapshots);
-      for (let i = 0; i < snapshots.length; i++) {
-        if (snapshots[i].exists()) {
-          console.log(snapshots[i].val());
-          values.push(snapshots[i].val());
-        } else {
-          console.log("No data available");
+    const values2: Promise<number[][]> = Promise.all(promise_array).then(
+      (snapshots) => {
+        console.log(snapshots);
+        for (let i = 0; i < snapshots.length; i++) {
+          if (snapshots[i].exists()) {
+            console.log(snapshots[i].val());
+            values.push(snapshots[i].val() as number[]);
+          } else {
+            console.log(`snapshot ${i} unavailable`);
+          }
         }
+        console.log(values);
+        return values;
       }
-      console.log(values);
-      return values;
+    );
+    return values2;
+  }
+
+  // Let n=x.length
+  // new_x_before=[0,1,2,...,x0-1]
+  // new_x_burst=[x0,x0+1,...,x1-1]
+  // new_x_after=[x1,x1+1,...,n-1]
+
+  updatePlot(index: number, x0: number, x1: number) {
+    const { plots } = this.state as myState;
+    const plot_i_3_tuple = plots[index];
+    // at any moment, the concatenated x_vals and y_vals have same length: the signal length
+    const all_x_vals = plot_i_3_tuple[0].x
+      .concat(plot_i_3_tuple[1].x)
+      .concat(plot_i_3_tuple[2].x);
+    const all_y_vals = plot_i_3_tuple[0].y
+      .concat(plot_i_3_tuple[1].y)
+      .concat(plot_i_3_tuple[2].y);
+    
+    // partitioning the x_vals and y_vals into 3 parts described by x0,x1
+    const new_x_before = this.make_new_x_burst(0, x0);
+    const new_y_before = this.make_corr_y_burst(0, x0, all_y_vals);
+    const new_x_burst = this.make_new_x_burst(0, x1);
+    const new_y_burst = this.make_corr_y_burst(x0, x1, all_y_vals);
+    const new_x_after = this.make_new_x_burst(x1, all_x_vals.length);
+    const new_y_after = this.make_corr_y_burst(x1,all_x_vals.length,all_y_vals);
+    const data_elements_before = {
+      x: new_x_before,
+      y: new_y_before,
+      type: "scattergl",
+      name: `sig_${index}`,
+      marker: { color: "black" },
+    };
+    const data_elements_burst = {
+      x: new_x_burst,
+      y: new_y_burst,
+      type: "scattergl",
+      name: `sig_${index}`,
+      marker: { color: "red" },
+    };
+    const data_elements_after = {
+      x: new_x_after,
+      y: new_y_after,
+      type: "scattergl",
+      name: `sig_${index}`,
+      marker: { color: "black" },
+    };
+    const new_plot_i_3_tuple = [
+      data_elements_before,
+      data_elements_burst,
+      data_elements_after,
+    ];
+    plots[index] = new_plot_i_3_tuple;
+    this.setState({
+      plots: plots,
     });
   }
 
   render() {
-    return <div className="FirebaseComponent">nothing to show</div>;
+    const { plots } = this.state as myState;
+    if (plots.length > 0) {
+      return (
+        <div className="FirebaseComponent">
+          {plots.map((plotData, i) => (
+            <div>
+              <Plot
+                key={i}
+                data={[plotData[0], plotData[1], plotData[2]]}
+                layout={layout_params}
+                onSelected={
+                  (e) => {
+                    console.log(e);
+                    if (e.range == undefined) {
+                      return;
+                    }
+                    // console.log(data)
+                    const x0 = e.range.x[0];
+                    const x1 = e.range.x[1];
+                    console.log(x0);
+                    console.log(x1);
+                    if (x0 == undefined || x1 == undefined) {
+                      return;
+                    }
+                    this.updatePlot(i, x0, x1);
+                  }
+                }
+              />
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return <div className="FirebaseComponent">Loading...</div>;
+    }
   }
 }
 
